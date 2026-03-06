363dc06dc3b8c93a4b817de6b8e75e920b12ac94
User-140524 <naamismyname@gmail.com>
2026-03-06 18:11:26 +0530

    Update dashboard.js

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -1,1 +1,35 @@
+import { auth, db } from "./firebase-config.js";
+import {
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+import {
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const userInfo = document.getElementById("userInfo");
+const logoutBtn = document.getElementById("logoutBtn");
+
+onAuthStateChanged(auth, async (user) => {
+  if (!user) {
+    window.location.href = "index.html";
+    return;
+  }
+
+  const userRef = doc(db, "users", user.uid);
+  const userSnap = await getDoc(userRef);
+
+  if (userSnap.exists()) {
+    const userData = userSnap.data();
+    userInfo.textContent = `Logged in as ${userData.name} (${userData.email})`;
+  } else {
+    userInfo.textContent = `Logged in as ${user.email}`;
+  }
+});
+
+logoutBtn.addEventListener("click", async () => {
+  await signOut(auth);
+  window.location.href = "index.html";
+});
